22be24105631ba2b3415dcfb3180a351b5fa6da6
faberh12 <hernandezfabian828@gmail.com>
2024-09-27 19:53:48 -0500

    finishing creating the files

---
+++ src/components/Login.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import './Login.css';
+
+const Login = () => {
+  return (
+    <div className="login-container">
+      {/* Barra de navegación */}
+      <nav className="navbar">
+        <div className="logo" />
+        <ul className="nav-links">
+          <li>Inicio</li>
+          <li>Nosotros</li>
+          <li>Contacto</li>
+          <li>FAQ</li>
+        </ul>
+        <div className="auth-buttons">
+          <button className="login-btn">Log In</button>
+          <button className="signup-btn">Sign Up</button>
+        </div>
+      </nav>
+
+      {/* Contenido de login */}
+      <div className="login-content">
+      </div>
+    </div>
+  );
+};
+
+export default Login;
