eba0faa44b56c0938f239828b0e946085c452a8a
Andre02468 <andrea.ceballosg@autonoma.edu.co>
2024-10-03 22:49:16 -0500

    Header component added

---
+++ src/components/Header/UserAuth/UserAuth.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './UserAuth.css'
+
+export function UserAuth() {
+  return (
+    <div className="header-buttons">
+      <button className="count-btn"></button>
+      <button className="menu-btn"></button>
+    </div>
+  )
+}
