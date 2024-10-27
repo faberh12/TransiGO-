717811d5a66563a739304855ec8247abd8c305c9
faberh12 <hernandezfabian828@gmail.com>
2024-10-27 15:14:39 -0500

    in process contact page

--- src/components/molecules/Header/Header.jsx
+++ src/components/molecules/Header/Header.jsx
@@ -4,6 +4,7 @@ import { Link, useLocation } from 'react-router-dom';
 import { LoginRegister } from './LoginRegister/LoginRegister.jsx';
 import { UserAuth } from './UserAuth/UserAuth.jsx';
 
+
 export function Header() {
 
   const [session, setSession] = useState("");
@@ -16,7 +17,7 @@ export function Header() {
       <nav className="nav-links">
         <li className={router.pathname == '/' ? 'current' : ''}><Link to={"/"}>Inicio</Link></li>
         <li className={router.pathname == '/user' ? 'current' : ''}><Link to={"/user"}>Nosotros</Link></li>
-        <li className={router.pathname == '/contacto' ? 'current' : ''}><Link to={"/"}>Contacto</Link></li>
+        <li className={router.pathname == '/Contact' ? 'current' : ''}><Link to={"/Contact"}>Contacto</Link></li>
         <li className={router.pathname == '/FAQ' ? 'current' : ''}><Link to={"/FAQ"}>FAQ</Link></li>
       </nav>
       {session? <UserAuth/> : <LoginRegister/>}
---
+++ src/components/pages/Contact/Contact.jsx
@@ -0,0 +1,11 @@
+// src/components/Contact/Contact.js
+import React from 'react';
+
+export function Contact() {
+  return (
+    <div>
+      <h1>Contacto</h1>
+      <p>Información de contacto aquí.</p>
+    </div>
+  );
+}
